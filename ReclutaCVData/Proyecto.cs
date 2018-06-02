1d589fc1de4bd908fd1e1dd5c904aea2af518613
Josè Luis Lopez <jllp_chepe@hotmail.com>
2018-06-01 19:49:02 -0700

    Clase para identificar experiencia del candidato.

---
+++ ReclutaCVData/Proyecto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReclutaCVData
+{
+    public enum RolFungidoProyecto
+    {
+        LiderTecnico,
+        AdministradorProyecto,
+        IngenieroSoftware
+    }   
+
+    public class Proyecto
+    {
+        public int TipoProyecto { get; set; }
+        public int DuracionEnMeses { get; set; }
+        public string LenguajeDeProgramacion { get; set; }
+        public int NumeroDeIntegrantes { get; set; }
+        public string RolFungido { get; set; }
+
+    }
+}
