19f0d9b79f9fae0ec9e61a0515b146b4be9e18f1
José Luis López <jllp_chepe@hotmail.com>
2018-06-20 20:46:47 -0700

    Base de Datos de Candidatos

---
+++ ReclutaCVData/Db.cs
@@ -0,0 +1,35 @@
+﻿using ReclutaCVData.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReclutaCVData
+{
+    /// <summary>
+    /// Contexto para controlar las entidades del sistema
+    /// </summary>
+    public class Db : DbContext
+    {
+
+        public Db() 
+            : base("Data Source=localhost;Initial Catalog=develop_reclutacv;Integrated Security=False;User Id=sa;Password=N0vut3k$C;")
+        {
+        }
+
+        public DbSet<Candidato> Candidato { get; set; }
+
+        public static void InitMigrations()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Db, ReclutaCVData.Migrations.Configuration>(true));
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ReclutaCVData/Entidades/Candidato.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReclutaCVData.Entidades
+{
+    public enum EstatusAcademico
+    {
+        Egresado,
+        Titulado,
+        Pasante,
+        Estudiante,
+        Trunca,
+    
+    }
+
+    /// <summary>
+    /// Un candidato a algun puesto
+    /// </summary>
+    public class Candidato
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        public int AñosDeExperiencia { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string CiudadResidencia { get; set; }
+        public decimal SueldoActual { get; set; }
+        public decimal SueldoEsperado { get; set; }
+        public string Carrera { get; set; }
+        public string Universidad { get; set; }
+
+        public decimal NivelDeInglesHablado { get; set; }
+        public decimal NivelDeInglesEscrito { get; set; }
+        public decimal NivelDeInglesLectura { get; set; }
+        public string Cursos { get; set; }
+        public string Certificaciones { get; set; }
+
+        //Datos de contacto
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+
+        public EstatusAcademico EstatusAcademico { get; set; } = EstatusAcademico.Estudiante;
+
+
+    }
+}
