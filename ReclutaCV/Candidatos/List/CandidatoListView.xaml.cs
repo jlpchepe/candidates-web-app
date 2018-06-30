afc3826fc1246f4bcd8369f98d73e85b7476ee0b
Josè Luis Lopez <jllp_chepe@hotmail.com>
2018-06-29 20:18:31 -0700

    Agregada list para ventanas de candatos

---
+++ ReclutaCV/Candidatos/List/CandidatoListView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReclutaCV.Candidatos.List
+{
+    /// <summary>
+    /// Lógica de interacción para CandidatoListView.xaml
+    /// </summary>
+    public partial class CandidatoListView : Window
+    {
+        public CandidatoListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
