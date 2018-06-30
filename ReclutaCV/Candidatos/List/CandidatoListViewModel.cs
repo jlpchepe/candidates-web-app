429e2df2593b64f1e25b885f1723e712f62a0ede
Josè Luis Lopez <jllp_chepe@hotmail.com>
2018-06-29 21:10:18 -0700

    Mejoras para candidato

--- ReclutaCV/Candidatos/List/CandidatoListViewModel.cs
+++ ReclutaCV/Candidatos/List/CandidatoListViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReclutaCVData.Entidades;
 
 namespace ReclutaCV.Candidatos.List
 {
     class CandidatoListViewModel
     {
+        public List<Candidato> Items { get; internal set; }
     }
 }
