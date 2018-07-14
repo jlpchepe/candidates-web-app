149e8e0fc529400d24986591084cfd91fcb0b0fc
Josè Luis Lopez <jllp_chepe@hotmail.com>
2018-07-13 21:04:33 -0700

    Repositorio de candidatos

---
+++ ReclutaCVData/Repositorios/CandidatoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReclutaCVData.Repositorios
+{
+    public class CandidatoRepository
+    {
+        public void FindAll() { }
+        public void FindById() { }
+        public void Update() { }
+        public void Insert() { }
+        public void Delete() { }
+    }
+}
