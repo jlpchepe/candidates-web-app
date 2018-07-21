048e4ab04b74ebee3f0f1d2adf227ed2e374e135
Josè Luis Lopez <jllp_chepe@hotmail.com>
2018-07-20 21:25:56 -0700

    Mètodos para obtener todos los candidatos, ID y dependencias del candidato en repositorio.

--- ReclutaCVData/Repositorios/CandidatoRepository.cs
+++ ReclutaCVData/Repositorios/CandidatoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReclutaCVData.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,28 @@ namespace ReclutaCVData.Repositorios
 {
     public class CandidatoRepository
     {
-        public void FindAll() { }
-        public void FindById() { }
+        public Db db { get; }
+
+        public CandidatoRepository(
+            Db db
+        )
+        {
+            this.db = db;
+        }
+
+        /// <summary>
+        /// Obtiene todos los candidatos existentes
+        /// </summary>
+        public List<Candidato> FindAll() {
+            return this.db.Candidato.ToList();
+        }
+
+        /// <summary>
+        /// Obtiene el candidato con el id especificado
+        /// </summary>
+        public Candidato FindById(int id) {
+            return this.db.Candidato.Find(id);
+        }
         public void Update() { }
         public void Insert() { }
         public void Delete() { }
