93e214c0be1784112b48ecded5e3cd835401865d
José Luis López <jllp_chepe@hotmail.com>
2018-08-24 22:33:56 -0700

    Mejoras para reporte de seguimiento

---
+++ ReclutaCVLogic/Reportes/ReporteDeSeguimientoACandidatos/MotivosDeDesinteresEnIniciarElProceso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReclutaCVLogic.Reportes.ReporteDeSeguimientoACandidatos
+{
+    public class MotivoDeDesinteresEnIniciarElProceso
+    {
+        public string MotivoDeDesinteresEnElProceso { get; set; }
+        public int CantidadDeCandidatos { get; set; }
+        
+        
+        
+        
+    }
+}
