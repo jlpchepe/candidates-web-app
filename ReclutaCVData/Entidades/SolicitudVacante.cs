bbef451bf5c22713e68bbe6aa2137f98489f425b
José Luis López <jllp_chepe@hotmail.com>
2018-09-14 20:08:41 -0700

    Cambios para la solicitud de vacantes

--- ReclutaCVData/Entidades/SolicitudVacante.cs
+++ ReclutaCVData/Entidades/SolicitudVacante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,11 @@ namespace ReclutaCVData.Entidades
 {
     public class SolicitudVacante
     {
+        [Key]
+        public int Id { get; set; }
+
         public int CantidadDeVacantes { get; set; }
         public RolCandidato RolVacante { get; set; }
         public DateTime FechaDeSolicitud { get; set; }
-
-
-      
     }
 }
